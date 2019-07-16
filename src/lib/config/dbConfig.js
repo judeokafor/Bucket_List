3d3588b8fcf7f9e7896217d3ad074609e069a1b0
Okafor Jude <Okaforjudechukwuebuka@gmail.com>
2019-07-16 22:33:33 +0100

    ch- set up babel, eslint and passport

---
+++ src/lib/config/dbConfig.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import env from './env';
+
+mongoose
+	.connect(env.DATABASE_URL, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+	})
+	.then(() => {
+		console.log(`MongoDB connected to ${env.DATABASE_URL}`);
+	})
+	.catch(err => {
+		console.log(err);
+	});
