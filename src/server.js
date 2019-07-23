1f7484007610362fdd026c3ee748733ad4320f9d
Okafor Jude <Okaforjudechukwuebuka@gmail.com>
2019-07-23 05:31:36 +0100

    add static page

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ import bucklists from './routes/bucketlist';
 dotenv.config();
 const app = express();
 
-app.use(express.static('../dist'));
+app.use(express.static('./dist'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
